5b5598ab35bb0d3d7aafbf1cb2603c4c15a5dce7
Zhilong Yang <yzl_200@sina.com>
2021-04-13 20:49:59 +0800

    add country ngrx

--- bioworld-app/src/app/app.module.ts
+++ bioworld-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '@src/environments/environment';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -25,10 +27,17 @@ const APP_DATE_FORMATS: MatDateFormats = {
 // Services
 import { NotificationModule } from './services';
 
+// Store
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+const StoreDevtools = !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [];
+import { reducers, effects } from './store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { environment } from '@src/environments/environment';
+
 import { HeaderComponent } from './components/header/header.component';
 
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -56,6 +65,16 @@ import { HeaderComponent } from './components/header/header.component';
     AngularFireStorageModule,
 
     MatNativeDateModule,
+
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    EffectsModule.forRoot(effects),
+    StoreDevtools,
+
     NotificationModule.forRoot()
   ],
   providers: [
