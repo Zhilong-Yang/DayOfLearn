db1466e3142a889542e232cbc6ca902a114e5139
zhilong yang <yzl_200@163.com>
2021-04-13 10:01:37 +0800

    from validator

--- bioworld-app/src/app/pages/demo/pages/shared/shared.component.ts
+++ bioworld-app/src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { regex, regexErrors } from '@app/shared/utils';
 
 @Component({
   selector: 'app-shared',
@@ -10,18 +10,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SharedComponent implements OnInit {
   form: FormGroup;
   isInline: boolean;
-  constructor(private fb: FormBuilder) { }
+  regexErrors = regexErrors;
+
+  constructor(private fb: FormBuilder) {
+    this.isInline = true;
+   }
 
   ngOnInit(): void {
     this.form = this.fb.group({
       input: [null,
-      {
-        updateOn: 'blur',
-        validators: [
+        {
+          updateOn: 'blur',
+          validators: [
             Validators.required,
-            Validators.minLength(3)
-        ]
-      }]
+            Validators.minLength(3),
+            Validators.pattern(regex.numbers)
+          ]
+        }]
     });
   }
 
