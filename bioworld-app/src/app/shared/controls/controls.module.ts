b8cba10ca314e5f02bdb9f17f751046f6d9207a9
zhilong yang <yzl_200@163.com>
2021-04-13 15:44:32 +0800

    add autocomplete

--- bioworld-app/src/app/shared/controls/controls.module.ts
+++ bioworld-app/src/app/shared/controls/controls.module.ts
@@ -9,6 +9,7 @@ import { CheckboxesModule } from './checkboxes/checkboxes.module';
 import { RadiosModule } from './radios/radios.module';
 import { DateModule } from './date/date.module';
 import { DateRangeModule } from './date-range/date-range.module';
+import { AutocompleteModule } from './autocomplete/autocomplete.module';
 
 @NgModule({
   declarations: [],
@@ -22,6 +23,7 @@ import { DateRangeModule } from './date-range/date-range.module';
     RadiosModule,
     DateModule,
     DateRangeModule,
+    AutocompleteModule
   ],
   exports: [
     InputModule,
@@ -32,6 +34,7 @@ import { DateRangeModule } from './date-range/date-range.module';
     RadiosModule,
     DateModule,
     DateRangeModule,
+    AutocompleteModule
   ]
 })
 export class ControlsModule { }
