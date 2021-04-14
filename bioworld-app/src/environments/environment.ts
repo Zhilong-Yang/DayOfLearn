35009979df261369f7b7181526216fbdcd528c9c
zhilong yang <yzl_200@163.com>
2021-04-14 12:27:46 +0800

    add user ngrx

--- bioworld-app/src/environments/environment.ts
+++ bioworld-app/src/environments/environment.ts
@@ -13,6 +13,10 @@ export const environment = {
       storageBucket: "bioworld-app.appspot.com",
       messagingSenderId: "991107019501",
       appId: "1:991107019501:web:74a8b6875c2e4ba57d92e4"      
+    },
+    actionCodeSettings: {
+        url: 'http://localhost:5200/demo',
+        handleCodeInApp: true
     }
   }
 };
