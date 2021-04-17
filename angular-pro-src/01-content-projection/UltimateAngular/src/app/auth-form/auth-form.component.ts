04d5374ec982a3712dae3c965bd899755f070f1c
Zhilong Yang <yzl_200@sina.com>
2021-04-17 16:32:33 +0800

    04-content-child-aftercontentinit

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/auth-form/auth-form.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/auth-form/auth-form.component.ts
@@ -1,13 +1,16 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ContentChild, AfterContentInit, EventEmitter, OnInit, Output} from '@angular/core';
 import {User} from './auth-form.interface';
+import {AuthRememberComponent} from './auth-remember/auth-remember.component';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements OnInit, AfterContentInit {
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+  showMessage = false;
+  @ContentChild(AuthRememberComponent) remember?: AuthRememberComponent;
 
   constructor() {
   }
@@ -15,7 +18,14 @@ export class AuthFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(value: User): void{
+  ngAfterContentInit(): void {
+    if (this.remember) {
+      this.remember.checked.subscribe((checked: boolean) =>
+        this.showMessage = checked);
+    }
+  }
+
+  onSubmit(value: User): void {
     this.submitted.emit(value);
   }
 }
