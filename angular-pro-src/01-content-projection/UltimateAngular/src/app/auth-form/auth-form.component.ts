a560ccf43497c5746da63bfe61414d9eaf607be4
Zhilong Yang <yzl_200@sina.com>
2021-04-17 18:04:03 +0800

    06-viewchild-afterviewinit

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/auth-form/auth-form.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/auth-form/auth-form.component.ts
@@ -1,21 +1,37 @@
-import {Component, ContentChildren, QueryList, AfterContentInit, EventEmitter, OnInit, Output} from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  EventEmitter,
+  OnInit,
+  Output
+} from '@angular/core';
 import {User} from './auth-form.interface';
 import {AuthRememberComponent} from './auth-remember/auth-remember.component';
+import {AuthMessageComponent} from '../auth-message/auth-message.component';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit, AfterContentInit {
+export class AuthFormComponent implements OnInit, AfterContentInit, AfterViewInit {
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-  showMessage = false;
   @ContentChildren(AuthRememberComponent) remember?: QueryList<AuthRememberComponent>;
+  @ViewChild(AuthMessageComponent, {static: true}) message?: AuthMessageComponent;
+  showMessage = false;
 
   constructor() {
+
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
   }
 
   ngAfterContentInit(): void {
@@ -23,6 +39,10 @@ export class AuthFormComponent implements OnInit, AfterContentInit {
       this.remember.forEach((item) => {
         item.checked.subscribe((checked: boolean) => this.showMessage = checked);
       });
+    }
+
+    if (this.message) {
+      this.message.days = 30;
     }
   }
 
