b1ffa9e5bf5f63a1ff5f4d37c8cb3d8a24ed1c6e
Zhilong Yang <yzl_200@sina.com>
2021-04-18 14:16:45 +0800

    21-changedetectionstrategy

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.Default,
   template: `
     <div>
-      <app-example-one></app-example-one>
-      <app-example-two></app-example-two>
-      <app-example-three></app-example-three>
+      <button (click)="addProp()">Add property</button>
+      <button (click)="changeUser()">Change user object</button>
+      <button (click)="changeName()">Change name property</button>
+      <div class="users">
+        <app-example-one [user]="user"></app-example-one>
+        <app-example-two [user]="user"></app-example-two>
+      </div>
     </div>
   `
 })
 export class AppComponent {
+  user: any = {
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  };
+
+  addProp(): void {
+    this.user.email = 'blink@blink-182.net';
+  }
+
+  changeName(): void {
+    this.user.name = 'Travis Barker';
+  }
+
+  changeUser(): void {
+    this.user = {
+      name: 'Tom Delonge',
+      age: 41,
+      location: 'California'
+    };
+  }
 }
