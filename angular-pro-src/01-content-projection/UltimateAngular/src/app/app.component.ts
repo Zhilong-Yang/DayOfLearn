3014a873b01d5686b8bda76f104938ff5829e409
Zhilong Yang <yzl_200@sina.com>
2021-04-18 18:49:15 +0800

    29-reactive-setup

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -1,47 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FileSizePipe} from './filesize.pipe';
-
-interface File {
-  name: string;
-  size: any;
-  type: string;
-}
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <div *ngFor="let file of mapped">
-        <p>{{ file.name }}</p>
-        <p>{{ file.size }}</p>
-      </div>
+      <app-stock-inventory></app-stock-inventory>
     </div>
-  `,
-  providers: [
-    FileSizePipe
-  ]
+  `
 })
-export class AppComponent implements OnInit {
-  files: File[] = [];
-  mapped: File[] = [];
-
-  constructor(
-    private fileSizePipe: FileSizePipe
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.files = [
-      {name: 'logo.svg', size: 2120109, type: 'image/svg'},
-      {name: 'banner.jpg', size: 18029, type: 'image/jpg'},
-      {name: 'background.png', size: 1784562, type: 'image/png'}
-    ];
-    this.mapped = this.files.map(file => {
-      return {
-        name: file.name,
-        type: file.type,
-        size: this.fileSizePipe.transform(file.size, 'mb')
-      };
-    });
-  }
-}
+export class AppComponent {}
