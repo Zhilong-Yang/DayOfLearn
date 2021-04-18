1dfc5a65e294e85591e6b3fcdaca87f34f266e06
Zhilong Yang <yzl_200@sina.com>
2021-04-18 14:04:19 +0800

    20-viewencapsulation-shadow-dom

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -1,33 +1,14 @@
-import {
-  Component,
-  ViewContainerRef,
-  ViewChild,
-  AfterViewInit,
-  TemplateRef,
-  ChangeDetectorRef
-} from '@angular/core';
-
-import {AuthFormComponent} from './auth-form/auth-form.component';
-
-import {User} from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <ng-container
-        [ngTemplateOutlet]="tmpl"
-        [ngTemplateOutletContext]="ctx">
-      </ng-container>
-      <ng-template #tmpl let-name let-location="location">
-        {{ name }} : {{ location }}
-      </ng-template>
+      <app-example-one></app-example-one>
+      <app-example-two></app-example-two>
+      <app-example-three></app-example-three>
     </div>
   `
 })
 export class AppComponent {
-  ctx = {
-    $implicit: 'Todd Motto',
-    location: 'England, UK'
-  };
 }
