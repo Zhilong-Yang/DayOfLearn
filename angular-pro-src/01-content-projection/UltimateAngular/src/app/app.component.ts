0c0225145ebb132ee6e6b2fc68b2d866bfb838b5
Zhilong Yang <yzl_200@sina.com>
2021-04-18 17:32:18 +0800

    pip parameter

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -12,7 +12,7 @@ interface File {
     <div>
       <div *ngFor="let file of files">
         <p>{{ file.name }}</p>
-        <p>{{ file.size | filesize }}</p>
+        <p>{{ file.size | filesize:'mega bytes' }}</p>
       </div>
     </div>
   `
