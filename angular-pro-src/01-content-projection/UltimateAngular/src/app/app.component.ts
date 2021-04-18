e82962514054aefc4dd4407afc2011e9662c1bcc
Zhilong Yang <yzl_200@sina.com>
2021-04-18 13:06:26 +0800

    16-template-viewcontainerref

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -3,9 +3,8 @@ import {
   ViewContainerRef,
   ViewChild,
   AfterViewInit,
-  ComponentFactoryResolver,
-  ChangeDetectorRef,
-  ComponentRef
+  TemplateRef,
+  ChangeDetectorRef
 } from '@angular/core';
 
 import {AuthFormComponent} from './auth-form/auth-form.component';
@@ -16,51 +15,27 @@ import {User} from './auth-form/auth-form.interface';
   selector: 'app-root',
   template: `
     <div>
-      <button (click)="destroyComponent()">
-        Destroy
-      </button>
-      <button (click)="moveComponent()">
-        Move
-      </button>
       <div #entry></div>
+      <ng-template #tmpl>
+        Todd Motto : England, UK
+      </ng-template>
     </div>
   `
 })
 export class AppComponent implements AfterViewInit {
 
-  @ViewChild('entry', {read: ViewContainerRef}) entry?: ViewContainerRef;
-  component?: ComponentRef<AuthFormComponent>;
+  // @ts-ignore
+  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
+  // @ts-ignore
+  @ViewChild('tmpl') tmpl: TemplateRef<any>;
 
   constructor(
-    private resolver: ComponentFactoryResolver,
     private cdr: ChangeDetectorRef
   ) {
   }
 
   ngAfterViewInit(): void {
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    // @ts-ignore
-    this.entry.createComponent(authFormFactory);
-    this.component = this.entry?.createComponent(authFormFactory, 0);
-    // @ts-ignore
-    this.component.instance.title = 'Create account';
-    this.component?.instance.submitted.subscribe(this.loginUser);
+    this.entry.createEmbeddedView(this.tmpl);
     this.cdr.detectChanges();
   }
-
-  destroyComponent(): void {
-    // @ts-ignore
-    this.component.destroy();
-  }
-
-  moveComponent(): void {
-    // @ts-ignore
-    // tslint:disable-next-line:no-non-null-assertion
-    this.entry.move(this.component!.hostView, 1);
-  }
-
-  loginUser(user: User): void {
-    console.log('Login', user);
-  }
-
 }
