71afeb328a5a2155c7eb757333ded4370ebe97b9
Zhilong Yang <yzl_200@sina.com>
2021-04-19 21:11:54 +0800

    41-controlvalueaccessor

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,7 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, forwardRef} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const COUNTER_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
 
 @Component({
   selector: 'app-stock-counter',
+  providers: [COUNTER_CONTROL_ACCESSOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
     <div class="stock-counter">
@@ -29,11 +37,28 @@ import {Component, Input} from '@angular/core';
 })
 export class StockCounterComponent {
 
+  // @ts-ignore
+  private onTouch: Function;
+  // @ts-ignore
+  private onModelChange: Function;
+
   @Input() step = 10;
   @Input() min = 10;
   @Input() max = 1000;
 
   value = 10;
+
+  // @ts-ignore
+  // tslint:disable-next-line:typedef
+  registerOnTouched(fn) {
+    this.onTouch = fn;
+  }
+
+  // @ts-ignore
+  // tslint:disable-next-line:typedef
+  registerOnChange(fn) {
+    this.onModelChange = fn;
+  }
 
   writeValue(value: number): void {
     this.value = value || 0;
@@ -42,12 +67,17 @@ export class StockCounterComponent {
   increment(): void {
     if (this.value < this.max) {
       this.value = this.value + this.step;
+      // @ts-ignore
+      this.onModelChange(this.value);
     }
   }
 
   decrement(): void {
     if (this.value > this.min) {
       this.value = this.value - this.step;
+      // @ts-ignore
+      this.onModelChange(this.value);
     }
+    this.onTouch();
   }
 }
