7be9924f8aa5c348d9182a67d25acb089bf6a0ec
Zhilong Yang <yzl_200@sina.com>
2021-04-20 20:08:31 +0800

    50-resolves

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/mail/components/mail-app/mail-app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/mail/components/mail-app/mail-app.component.ts
@@ -1,24 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'mail-app',
   styleUrls: ['mail-app.component.scss'],
   template: `
     <div class="mail">
-      <router-outlet
-        (activate)="onActivate($event)"
-        (deactivate)="onDeactivate($event)">
+      <router-outlet>
       </router-outlet>
     </div>
   `
 })
 export class MailAppComponent {
-  // @ts-ignore
-  onActivate(event) {
-    console.log('Activate:', event);
-  }
-  // @ts-ignore
-  onDeactivate(event) {
-    console.log('Deactivate:', event);
-  }
+
 }
---
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/mail/mail.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {Mail} from './models/mail.interface';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class MailService {
+  constructor(private http: HttpClient) {
+  }
+
+  getFolder(folder: string): Observable<Mail[]> {
+
+    return this.http
+      .get(`/assets/${folder}.json`)
+      .pipe(
+        map((res: any) => {
+          return res['messages'];
+        }));
+  }
+}
