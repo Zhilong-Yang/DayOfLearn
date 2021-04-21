4eb7b33f6710517531f5ec44d015a7d513461127
Zhilong Yang <yzl_200@sina.com>
2021-04-21 20:16:32 +0800

    80-observable-store

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.component.ts
@@ -5,20 +5,11 @@ import {Store} from './store';
   selector: 'app-root',
   template: `
     <div>
-      <div *ngFor="let todo of todos$ | async">
-        {{ todo.name }}
-      </div>
+      <songs-playlist></songs-playlist>
+      <songs-listened></songs-listened>
+      <songs-favourites></songs-favourites>
     </div>
   `
 })
 export class AppComponent {
-
-  todos$ = this.store.select<any[]>('todos');
-
-  constructor(private store: Store) {
-    this.store.set('todos',[
-      { id: 1, name: 'Eat dinner' },
-      { id: 2, name: 'Do washing' }
-    ]);
-  }
 }
--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.module.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {Store} from './store';
+import {SongsModule} from './songs/songs.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SongsModule
   ],
   providers: [
     Store
