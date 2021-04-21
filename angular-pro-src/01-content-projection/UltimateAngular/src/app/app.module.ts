19aeb869447173588914118f84f788d3ae9a85b4
zhilong yang <yzl_200@163.com>
2021-04-21 09:11:07 +0800

    59-canload-guard

--- angular-pro-src/01-content-projection/UltimateAngular/src/app/app.module.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {PreloadAllModules, PreloadingStrategy, Route, RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {MailModule} from './mail/mail.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {Observable} from "rxjs/Observable";
 import "rxjs-compat/add/observable/of";
-
-export class CustomPreload implements PreloadingStrategy {
-  preload(route: Route, fn: () => Observable<any>): Observable<any> {
-    return route.data && route.data.preload ? fn() : Observable.of(null);
-  }
-}
+import {AuthModule} from "./auth/auth.module";
+import {AuthGuard} from "./auth/auth.guard";
 
 export const ROUTES: Routes = [
-  {path: 'dashboard', data: { preload: true }, loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+  {path: 'dashboard', canLoad: [AuthGuard], loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path: '**', redirectTo: 'mail/folder/inbox'}
 ];
 
@@ -22,12 +17,13 @@ export const ROUTES: Routes = [
   declarations: [
     AppComponent
   ],
-  providers: [CustomPreload],
+
   imports: [
     BrowserModule,
     HttpClientModule,
     MailModule,
-    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload })
+    AuthModule,
+    RouterModule.forRoot(ROUTES)
   ],
   bootstrap: [
     AppComponent
--- angular-pro-src/01-content-projection/UltimateAngular/src/app/mail/components/mail-view/mail-view.component.ts
+++ angular-pro-src/01-content-projection/UltimateAngular/src/app/mail/components/mail-view/mail-view.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/pluck';
@@ -12,11 +12,37 @@ import {Mail} from '../../models/mail.interface';
       <h2>{{ (message | async)!.from }}</h2>
       <p>{{ (message | async)!.full }}</p>
     </div>
+    <div class="mail-reply">
+      <textarea
+        (change)="updateReply($event)"
+        placeholder="Type your reply..."
+        [value]="reply">
+      </textarea>
+      <button type="button" (click)="sendReply()">
+        Send
+      </button>
+    </div>
   `
 })
-export class MailViewComponent {
+export class MailViewComponent implements OnInit {
+  reply = '';
   message: Observable<Mail> = this.route.data.pluck('message');
 
   constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => {
+      this.reply = '';
+    });
+  }
+
+  // @ts-ignore
+  updateReply(event) {
+    this.reply = event.target.value;
+  }
+
+  sendReply() {
+    console.log('Sent!', this.reply);
   }
 }
